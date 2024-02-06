25ec35bdcfc4f00d43a55bc1b6db027b3a0b438e
Thales Senra <thalesmarinhotwn@hotmail.com>
2024-02-06 15:18:24 -0300

    feat: point repository

---
+++ src/main/java/org/xy/pointsofinterest/pointsofinterest/repository/PointRepository.java
@@ -0,0 +1,17 @@
+package org.xy.pointsofinterest.pointsofinterest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.xy.pointsofinterest.pointsofinterest.entity.Point;
+
+import java.util.List;
+
+@Repository
+public interface PointRepository extends JpaRepository<Point, Long> {
+
+    List<Point> findByName(String name);
+
+    @Query("delete from Point p where p.name = ?1")
+    void delete(String name);
+}
