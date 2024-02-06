28ea90e19b0cd0e071238e94518d227cc925ae4a
Thales Senra <thalesmarinhotwn@hotmail.com>
2024-02-06 14:24:49 -0300

    Initial setup

---
+++ src/main/java/org/xy/pointsofinterest/pointsofinterest/PointsOfInterestApplication.java
@@ -0,0 +1,13 @@
+package org.xy.pointsofinterest.pointsofinterest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointsOfInterestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointsOfInterestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/xy/pointsofinterest/pointsofinterest/PointsOfInterestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xy.pointsofinterest.pointsofinterest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointsOfInterestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
