b8bfabac0b1ec9ed1051bdb5257c7a701523c572
Thales Senra <thalesmarinhotwn@hotmail.com>
2024-02-19 12:43:04 -0300

    fix: added @AllArgsConstructor and @NoArgsConstructor annotations

--- src/main/java/org/xy/pointsofinterest/pointsofinterest/entity/Point.java
+++ src/main/java/org/xy/pointsofinterest/pointsofinterest/entity/Point.java
@@ -2,10 +2,14 @@ package org.xy.pointsofinterest.pointsofinterest.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "point")
 @Builder
