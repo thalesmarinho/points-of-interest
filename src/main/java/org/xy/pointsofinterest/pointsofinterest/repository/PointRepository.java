ea745f63059b5a40a5e00db596b90a1a06c02c74
Thales Senra <thalesmarinhotwn@hotmail.com>
2024-02-19 12:46:48 -0300

    fix: added @Modifying and @Transactional annotations to delete method on PointRepository.java

--- src/main/java/org/xy/pointsofinterest/pointsofinterest/repository/PointRepository.java
+++ src/main/java/org/xy/pointsofinterest/pointsofinterest/repository/PointRepository.java
@@ -1,8 +1,10 @@
 package org.xy.pointsofinterest.pointsofinterest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import org.xy.pointsofinterest.pointsofinterest.entity.Point;
 
 import java.util.List;
@@ -12,6 +14,8 @@ public interface PointRepository extends JpaRepository<Point, Long> {
 
     List<Point> findByName(String name);
 
+    @Modifying
+    @Transactional
     @Query("delete from Point p where p.name = ?1")
     void delete(String name);
 
